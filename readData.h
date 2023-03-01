1cbb24c85f440c008e71e729c6e26b9d7ac452dd
damianlech <damian.lech11@gmail.com>
2023-03-01 10:50:10 +0100

    Added documentation about readData.h

--- readData.h
+++ readData.h
@@ -2,6 +2,12 @@
 
 #define READATA_H
 
+/*
+The purpose of these two functions is to:
+1. open and read /proc/stat using printLineFromFile to get a first line as a string
+2. convert that string to a usable integer array using convertStringToIntegerArray
+*/
+
 void printLineFromFile();
 
 void convertStringToIntegerArray();
